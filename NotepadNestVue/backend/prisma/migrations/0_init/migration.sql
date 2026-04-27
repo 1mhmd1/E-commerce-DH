3f2dffcc15b7f0fde457a6a97399f949a084ab8f
MhmdSalim <151769456+1mhmd1@users.noreply.github.com>
2026-04-27 03:29:57 +0300

    Delete NotepadNestVue directory

--- NotepadNestVue/backend/prisma/migrations/0_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Note" (
-    "id" SERIAL NOT NULL PRIMARY KEY,
-    "title" VARCHAR(120) NOT NULL,
-    "content" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL
-);
--- NotepadNestVue/backend/src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { NotesModule } from './notes/notes.module';
-
-@Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), NotesModule],
-})
-export class AppModule {}
